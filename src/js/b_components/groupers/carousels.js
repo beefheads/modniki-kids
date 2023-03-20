a88f4177b1934c2ac8263c034f9722fda815e1c6
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-03-20 11:52:42 +0300

    Переорганизует js

--- src/js/b_components/groupers/carousels.js
+++ src/js/b_components/groupers/carousels.js
@@ -1,5 +1,29 @@
 "use strict"
 
+
+/*
+import Swiper, { Navigation, Pagination } from "swiper";
+
+const mainSwiper = new Swiper("[data-swiper=main]", {
+    modules: [ Navigation, Pagination ],
+
+ slidesPerView: 1,
+ spaceBetween: 24,
+ loop: true,
+
+    navigation: {
+        nextEl: ".main-swiper__arrow.is-next",
+        prevEl: ".main-swiper__arrow.is-prev",
+    },
+
+ pagination: {
+   el: '.main-swiper__pagin',
+   type: 'bullets',
+   clickable: true,
+ },
+});
+*/
+
 import Swiper, { Navigation, Autoplay, Pagination, Thumbs, EffectFade, Grid } from "swiper";
 import {debounce} from "../utils/helpers.js";
 
